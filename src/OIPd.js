3f9c79c405d6e3533c7bb39b99441b792c2e4672
OstlerDev <OstlerDev@users.noreply.github.com>
2017-12-12 13:59:23 -0800

    Add OIPd Squash Merge functions :)

--- src/OIPd.js
+++ src/OIPd.js
@@ -1,7 +1,46 @@
+import jsonpatch from 'fast-json-patch';
+
 var OIPdFunction = function(){
 	var OIPd = {}
 
-	
+	OIPd.createSquashedPatch = function(original, modified){
+
+	}
+
+	OIPd.squashPatch = function(patch){
+		var squashed = {};
+
+		for (var i = 0; i < patch.length; i++) {
+			// Store the operation
+			var operation = patch[i].op;
+			// Remove operation key from squashed patch
+			delete patch[i].op;
+			// Check what the operation is, and put it in the right place
+			if (!squashed[operation])
+				squashed[operation] = [];
+			
+			squashed[operation].push(patch[i]);
+		}
+		
+		return squashed;
+	}
+
+	OIPd.unSquashPatch = function(squashedPatch){
+		var patch = [];
+
+		for (var op in squashedPatch){
+			for (var i = 0; i < squashedPatch[op].length; i++) {
+				// Load what we saved from the patch
+				var singlePatch = squashedPatch[op];
+				// Restore the operation to the patch
+				singlePatch.op = op;
+				// Add singlePatch to patch
+				patch.push(singlePatch);
+			}
+		}
+
+		return patch;
+	}
 
 	this.OIPd = OIPd;
 	return this.OIPd;
